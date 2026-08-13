d951cfc79d6666abe5d6f5f93279073e3d43b756
websak <websak@yahoo.co.uk>
2026-08-13 12:10:49 +0100

    rename theme settings json file

--- gt_system/themes/growth_tech/inc/register-blocks.php
+++ gt_system/themes/growth_tech/inc/register-blocks.php
@@ -15,16 +15,16 @@ function register_acf_block_types() {
 
 
 	// Two Column Content With Stats
-	acf_register_block_type(array(
-		'name'				=> 'card-two-column-stats',
-		'title'				=> __('Two Columns With Stats'),
-		'description'		=> __('Two Columns With Stats'),
-		'render_template'	=> 'template-parts/blocks/card-two-column-with-stats.php',
-		'category'			=> 'formatting',
-		'mode'	=> 'edit',
-		'supports' => array('mode' => false, 'anchor' => true),
-		'icon'				=> 'layout'
-	));
+	// acf_register_block_type(array(
+	// 	'name'				=> 'card-two-column-stats',
+	// 	'title'				=> __('Two Columns With Stats'),
+	// 	'description'		=> __('Two Columns With Stats'),
+	// 	'render_template'	=> 'template-parts/blocks/card-two-column-with-stats.php',
+	// 	'category'			=> 'formatting',
+	// 	'mode'	=> 'edit',
+	// 	'supports' => array('mode' => false, 'anchor' => true),
+	// 	'icon'				=> 'layout'
+	// ));
 
 }
 
@@ -32,6 +32,12 @@ if( function_exists('acf_register_block_type') ) {
 	add_action('acf/init', 'register_acf_block_types');
 }
 
+
+//Keep hand-named acf-json files instead of reverting to the group key
+add_filter( 'acf/json/save_file_name', 'gt_acf_json_file_name', 10, 3 );
+function gt_acf_json_file_name( $filename, $post, $load_path ) {
+	return $load_path ? basename( $load_path ) : $filename;
+}
 
 add_filter( 'acf/the_field/escape_html_optin', '__return_true' );
 add_action( 'acf/init', 'set_acf_settings' );
--- gt_system/themes/growth_tech/page-templates/page-home.php
+++ gt_system/themes/growth_tech/page-templates/page-home.php
@@ -4,25 +4,6 @@ get_header();
 ?>
 
 
-<?php $banner = get_field("homepage_banner");?>
-
-<section class="main-slide" <?php if($banner['image']) :?> style="background-image:url('<?php echo $banner['image']['url'];?>"><?php endif;?>
-	<div class="container">
-		<div class="row">
-		<div class="inner">
-			<div class="content">        
-				<h1><?php echo $banner['title'];?></h1>
-				<?php echo $banner['text'];?>
-				<?php foreach($banner['buttons'] as $button):?>
-					<a href="<?php echo $button['button_link'];?>" class="btn white" <?php if($button['external_link'] === TRUE) : ?> target="_blank" <?php endif;?>><?php echo $button['button_title'];?></a>
-				<?php endforeach;?>
-			</div>
-		</div>
-		</div>
-	</div>
-</section>
-
-
 <?php if(have_posts()): 
 		while(have_posts()): the_post(); ?>
 <?php the_content(); ?>
